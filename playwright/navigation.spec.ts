664c709b7b6600e180391f5f8ccc0e7a20a3b0d9
Mandla <127042808+th0Rn4Do@users.noreply.github.com>
2024-03-27 23:10:49 +0100

    Add test case for search button to be visible

--- playwright/navigation.spec.ts
+++ playwright/navigation.spec.ts
@@ -4,4 +4,6 @@ test('navigation test', async ({ page }) => {
   await page.goto('http://localhost:3000/');
 
   await expect(page.getByRole('link', { name: 'Home' })).toBeVisible();
+
+  await expect(page.getByRole('link', { name: 'Search' })).toBeVisible();
 });
