f268fbf5441f90d23d5afa4169d004402c39ccc7
Mandla <127042808+th0Rn4Do@users.noreply.github.com>
2024-03-28 19:06:17 +0100

    Remove localhost from page.goto so playwright works

--- playwright/navigation.spec.ts
+++ playwright/navigation.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 
 test('navigation test', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('/');
 
   await expect(page.getByRole('link', { name: 'Home' })).toBeVisible();
 
